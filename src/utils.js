cebbca33830547f750f9d5d140340576d7338e09
sbishop <sbishop@navarik.com>
2018-12-03 12:53:03 -0800

    Test commit.

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
 export const maybe = f => x => (x === undefined || x === null ? x : f(x))
 export const head = (xs) => { for (let x of xs) return x }
 export const liftToArray = f => x => (x instanceof Array ? x.map(f) : f(x))
+
+// test commit
