fb07af22f8ed1172f7a9409ba19b8ddd0b4f4eff
Your Name <achernikov@navarik.com>
2020-01-02 18:14:02 -0800

    Savepoint

--- src/local-state/ne-db-index-adapter.ts
+++ src/local-state/ne-db-index-adapter.ts
@@ -1,61 +1,40 @@
 import * as Database from 'nedb'
-import { Dictionary } from '@navarik/types'
-import { convertSortQueriesToPairs } from './convert-sort-query-to-pairs'
-
-// TODO Plug these in correctly
-// const escape = str => str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
-// const makeRegex = (str, pre='', suf='') => new RegExp(`${pre}${escape(str)}${suf}`, 'i')
-
-// export const findSubstring = str => makeRegex(str)
-// export const findPrefix = str => makeRegex(str, '^\\s*')
-// export const findSuffix = str => makeRegex(str, '', '\\s*$')
+import { SearchIndexAdapter, SearchQuery, SearchOptions } from '../types'
+import { parseSortQuery, prepareSearch } from './query-language'
 
 const databaseError = (err) => {
   throw new Error(`[NeDB] Database error: ${err}`)
 }
 
-const customTerms = v => {
-  if (v instanceof RegExp) {
-    return { $regex: v }
-  }
-
-  return v
-}
-
-const prepareSearch = searchParams => Object.entries(searchParams || {}).reduce((acc, [k, v]) => ({ ...acc, [k]: customTerms(v) }), {})
-
-export class NeDbIndexAdapter {
-  private client
+export class NeDbIndexAdapter implements SearchIndexAdapter {
+  private client: Database
 
   constructor() {
-    this.reset()
+    this.client = new Database()
+    this.client.ensureIndex({ fieldName: 'id', unique: true })
   }
 
-  find(searchParams, options: Dictionary<any> = {}) {
+  find(searchParams: SearchQuery, { offset, limit, sort }: SearchOptions = {}) {
+    const query = this.client.find(prepareSearch(searchParams), { _id: 0 })
+
+    if (offset) {
+      query.skip(offset)
+    }
+    if (limit) {
+      query.limit(limit)
+    }
+    if (sort) {
+      // Translate the array of sort queries from CoreQL format to NeDB cursor.sort() format. Example:
+      //    received this:         [ 'vessels:asc', 'foo.bar.baz:desc', ... ]
+      //    helper function makes: [ ['vessels', 1], ['foo.bar.baz', -1], ...]
+      //    NeDB wants this:       { vessels: 1 , 'foo.bar.baz': -1, ... }
+      const nedbSortingObject = parseSortQuery(sort)
+        .reduce((acc, { field, direction }) => ({ ...acc, [field]: direction }), {})
+
+      query.sort(nedbSortingObject)
+    }
+
     return new Promise((resolve, reject) => {
-      const query = this.client.find(prepareSearch(searchParams), { _id: 0 })
-
-      const offset = parseInt(options.offset, 10)
-      if (Number.isInteger(offset)) {
-        query.skip(offset)
-      }
-
-      const limit = parseInt(options.limit, 10)
-      if (Number.isInteger(limit)) {
-        query.limit(limit)
-      }
-
-      if (options.sort) {
-        // Translate the array of sort queries from Express format to NeDB cursor.sort() format. Example:
-        //    received this:         [ 'vessels:asc', 'foo.bar.baz:desc', ... ]
-        //    helper function makes: [ ['vessels', 1], ['foo.bar.baz', -1], ...]
-        //    NeDB wants this:       { vessels: 1 , 'foo.bar.baz': -1, ... }
-        const nedbSortingObject = {}
-        convertSortQueriesToPairs(options.sort).map(pair => nedbSortingObject[pair[0]] = pair[1])
-
-        query.sort(nedbSortingObject)
-      }
-
       query.exec((err, res) => {
         if (err) reject(databaseError(err))
         else resolve(res || [])
@@ -63,7 +42,7 @@ export class NeDbIndexAdapter {
     })
   }
 
-  count(searchParams) {
+  count(searchParams: SearchQuery) {
     return new Promise((resolve, reject) => {
       this.client.count(prepareSearch(searchParams), (err, res) => {
         if (err) reject(databaseError(err))
@@ -72,7 +51,7 @@ export class NeDbIndexAdapter {
     })
   }
 
-  insert(documents) {
+  index(documents) {
     return new Promise((resolve, reject) =>
       this.client.insert(documents, (err, res) => {
         if (err) reject(databaseError(err))
@@ -90,11 +69,9 @@ export class NeDbIndexAdapter {
     )
   }
 
-  reset() {
+  async reset() {
     this.client = new Database()
     this.client.ensureIndex({ fieldName: 'id', unique: true })
-
-    return Promise.resolve(true)
   }
 
   connect() {
--- src/local-state/query-language.ts
+++ src/local-state/query-language.ts
@@ -6,7 +6,7 @@
  * @param {string|string[]} sortQueries - A single sort query string or an array of sort query strings in descending priority.
  * @returns {Array<Array>} - An array of one or more [string, number] pairs where string is the field to be sorted by and number is either 1 for ascending sorting or -1 for descending sorting.
  */
-export const convertSortQueriesToPairs = (sortQueries) => {
+export const parseSortQuery = (sortQueries) => {
   // Convert sortQueries to an array if it is singular.
   sortQueries = sortQueries.constructor !== Array ? [sortQueries] : sortQueries
   // Translate an array of sort queries to an array of [string, number] pairs.
@@ -15,7 +15,25 @@ export const convertSortQueriesToPairs = (sortQueries) => {
     const sortPieces = sortQuery.split(':')
     const sortField = sortPieces.shift()
     const sortOrder = sortPieces.includes('desc') ? -1 : 1
-    arrayOfSorts.push([sortField, sortOrder])
+    arrayOfSorts.push({ field: sortField, direction: sortOrder })
   });
   return arrayOfSorts
 }
+
+const customTerms = v => {
+  if (v instanceof RegExp) {
+    return { $regex: v }
+  }
+
+  return v
+}
+
+export const prepareSearch = (searchParams) => Object.entries(searchParams || {}).reduce((acc, [k, v]) => ({ ...acc, [k]: customTerms(v) }), {})
+
+// TODO Plug these in correctly
+// const escape = str => str.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&')
+// const makeRegex = (str, pre='', suf='') => new RegExp(`${pre}${escape(str)}${suf}`, 'i')
+
+// export const findSubstring = str => makeRegex(str)
+// export const findPrefix = str => makeRegex(str, '^\\s*')
+// export const findSuffix = str => makeRegex(str, '', '\\s*$')
