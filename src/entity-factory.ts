33c3ffa1929284e0184ee55749921cd9639b7931
Anthony Chernikov <anthony@acvos.com>
2020-06-08 12:10:10 -0700

    Validation error

--- src/entity-factory.ts
+++ src/entity-factory.ts
@@ -2,6 +2,7 @@ import { v5 as uuidv5, v4 as uuidv4 } from 'uuid'
 import { Dictionary } from '@navarik/types'
 import { CoreDdl } from '@navarik/core-ddl'
 import { IdGenerator, CanonicalEntity, EntityData, UUID, Document } from './types'
+import { ValidationError } from "./validation-error"
 
 type FactoryConfig = {
   generators: Dictionary<IdGenerator>
@@ -36,7 +37,7 @@ export class EntityFactory<B extends Document, M extends Document> {
 
     const { isValid, message } = this.ddl.validate(data.type, data.body)
     if (!isValid) {
-      throw new Error(`[Storage] Validation failed for ${data.type}. ${message}`)
+      throw new ValidationError(`[Storage] Validation failed for ${data.type}. ${message}`)
     }
 
     const formatted = this.ddl.format(type, body)
