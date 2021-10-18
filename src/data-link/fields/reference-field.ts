e17f00b29eb725c787ce29ed59c18bc9db80831a
Anton <achernikov@navarik.com>
2021-10-18 15:06:18 -0700

    Not all references are required

--- src/data-link/fields/reference-field.ts
+++ src/data-link/fields/reference-field.ts
@@ -1,21 +1,31 @@
-import { ValidatableField, EntityRegistry } from "../../types"
+import { ValidatableField, EntityRegistry, SchemaField } from "../../types"
 
 interface Config {
   path: string
   state: EntityRegistry<any>
+  field: SchemaField<{}>
 }
 
 export class ReferenceField implements ValidatableField {
   private name: string
   private state: EntityRegistry<any>
+  private isRequired: boolean
 
-  constructor({ state, path }: Config) {
+  constructor({ state, path, field: { required } }: Config) {
     this.name = path
     this.state = state
+    this.isRequired = !!required
   }
 
-  async validate(value: any) {
-    if (!await this.state.has(value)) {
+  async validate(value: any = null) {
+    if (value === null && !this.isRequired) {
+      return {
+        isValid: true,
+        message: ""
+      }
+    }
+
+    if (this.isRequired && !await this.state.has(value)) {
       return {
         isValid: false,
         message: `Reference document "${value}" not found for "${this.name}". `
