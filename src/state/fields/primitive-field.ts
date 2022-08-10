b7d2c7537a11e6fe6452d033bb7164ab21d1d0d5
Anton Chernikov <75864857+nav-achernikov@users.noreply.github.com>
2022-08-10 14:22:50 -0700

    Allow accepting string values for non-string fields in search queries (#97)

--- src/state/fields/primitive-field.ts
+++ src/state/fields/primitive-field.ts
@@ -2,11 +2,22 @@ import { SchemaField, SearchableField, SearchQuery } from "../../types"
 import { CompilerError } from "../../errors/compiler-error"
 import { FieldFactory } from "../field-factory"
 
+const typeConvertors = {
+  int: parseInt,
+  float: parseFloat,
+  double: parseFloat,
+  string: x => `${x}`,
+  datetime: x => new Date(x),
+  default: x => x
+}
+
 export class PrimitiveField implements SearchableField {
   public type: string
+  private convertor: (x: any) => any
 
   constructor(factory: FieldFactory, field: SchemaField) {
     this.type = field.type
+    this.convertor = typeConvertors[this.type] || typeConvertors.default
   }
 
   chain(field: SchemaField) {
@@ -24,6 +35,11 @@ export class PrimitiveField implements SearchableField {
       return false
     }
 
-    return query
+    const { operator, args: [field, value] } = query
+    const formattedValue = value instanceof Array
+      ? value.map(x => this.convertor(x))
+      : this.convertor(value)
+
+    return { operator, args: [field, formattedValue] }
   }
 }
