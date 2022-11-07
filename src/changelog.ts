93122785752373a8015367087f1d30a8ba22ac26
Orlando Frade <ofrade@navarik.com>
2022-11-07 10:41:14 -0800

    FTL-1-storage-change-event-deduplication1~

--- src/changelog.ts
+++ src/changelog.ts
@@ -1,12 +1,14 @@
 import { Logger } from '@navarik/types'
 import { TransactionManager } from "@navarik/transaction-manager"
 import { v4 as uuidv4 } from 'uuid'
+import LRU from "lru-cache"
 import { ChangelogAdapter, CanonicalEntity, ChangeEvent, EntityEnvelope, ActionType, CanonicalSchema } from './types'
 import { Entity } from './entity'
 
 interface ChangelogConfig<M extends object> {
   adapter: ChangelogAdapter<M>
   logger: Logger
+  cacheSize: number
   observer: (change: CanonicalEntity<any, M>, schema: CanonicalSchema) => Promise<void>
 }
 
@@ -15,18 +17,22 @@ export class Changelog<M extends object> {
   private observer: (change: CanonicalEntity<any, M>, schema: CanonicalSchema) => Promise<void>
   private transactionManager: TransactionManager
   private logger: Logger
+  private cache: LRU<string, CanonicalEntity<any, M>>
   private healthStats = {
     totalChangesProduced: 0,
     totalChangesReceived: 0,
     totalProcessingErrors: 0
   }
 
-  constructor({ observer, adapter, logger }: ChangelogConfig<M>) {
+  constructor({ observer, adapter, logger, cacheSize }: ChangelogConfig<M>) {
     this.logger = logger
     this.adapter = adapter
     this.observer = observer
     this.transactionManager = new TransactionManager()
     this.adapter.observe(x => this.onChange(x))
+    this.cache = new LRU({
+      max: cacheSize
+    })
   }
 
   private async onChange<B extends object>({ id, action, entity, schema }: ChangeEvent<B, M>) {
@@ -34,6 +40,10 @@ export class Changelog<M extends object> {
 
     try {
       this.logger.debug({ component: "Storage" }, `Received change event for entity: ${entity.id}`)
+
+      if (this.cache.has(id)) { 
+        return 
+      }
 
       // This makes Storage compatible with older CanonicalEntity type without the `last_action` field
       if (!entity.last_action) {
@@ -43,7 +53,8 @@ export class Changelog<M extends object> {
       await this.observer(entity, schema)
 
       this.transactionManager.commit(id, new Entity(entity).envelope())
-
+      this.cache.set(id, entity)
+      
     } catch (error: any) {
       this.healthStats.totalProcessingErrors++
       this.logger.error({ component: "Storage", stack: error.stack }, `Error processing change event: ${error.message}`)
@@ -73,6 +84,7 @@ export class Changelog<M extends object> {
 
   async down() {
     await this.adapter.down()
+    this.cache.clear()
   }
 
   async isHealthy() {
